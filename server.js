53b210d427dd70000dc9c5cd821ad7da32ae4401
TaysonDiego <thaysondiegoda1@gmail.com>
2026-05-08 01:21:51 -0300

    adicionando autenticação por token

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+const TOKEN = "X%BNY&9@1s8!A";
+
 app.use(express.json());
 
 app.get('/dados', (req, res) => {
@@ -11,7 +13,11 @@ app.get('/dados', (req, res) => {
 });
 
 app.post('/dados', (req, res) => {
+    const token = req.headers['authorization'];
 
+    if (token !== TOKEN) {
+        return res.status(401).send("Token de autenticação inválido");
+    }
     console.log("Dados recebidos:");
     console.log(req.body);
 
